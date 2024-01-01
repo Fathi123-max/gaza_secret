4450430812a32691a852a0d61df5b6eb8b44d47c
Fathi wehba <Fathiwehba5@gmail.com>
2024-01-01 15:44:16 +0200

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:gaza_secret/app/routes/app_pages.dart';
 import 'package:gaza_secret/app/translations/app_translations.dart';
 import 'package:get/get.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'app/ui/theme/app_theme.dart';
 
 void main() {
-  runApp(GetMaterialApp(
-    debugShowCheckedModeBanner: false,
-    initialRoute: Routes.INITIAL,
-    theme: appThemeData,
-    defaultTransition: Transition.fade,
-    getPages: AppPages.pages,
-    locale: Locale('pt', 'BR'),
-    translationsKeys: AppTranslation.translations,
-  ));
+  runApp(ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      // Use builder only if you need to use library outside ScreenUtilInit context
+      builder: (_, child) {
+        return GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          initialRoute: Routes.INITIAL,
+          theme: appThemeData,
+          defaultTransition: Transition.cupertino,
+          getPages: AppPages.pages,
+          locale: Locale('pt', 'BR'),
+          translationsKeys: AppTranslation.translations,
+        );
+      }));
 }
